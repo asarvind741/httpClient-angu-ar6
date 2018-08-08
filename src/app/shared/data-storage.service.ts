fb971ad2f665ea61265fba705a282698f86b17e0
Arvind Singh <arvindsingh741234@gmail.com>
2018-08-09 01:09:05 +0530

    first

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/Rx';
+
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class DataStorageService {
+  constructor(private http: Http,
+              private httpClient: HttpClient,
+              private recipeService: RecipeService,
+              private authService: AuthService) {
+  }
+
+  storeRecipes() {
+    const token = this.authService.getToken();
+
+    // return this.http.put('https://ng-recipe-book-3adbb.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
+    return this.httpClient.put('https://angular-recipe-4f395.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
+
+  }
+
+  getRecipes() {
+    const token = this.authService.getToken();
+
+    this.httpClient.get<Recipe[]>('https://angular-recipe-4f395.firebaseio.com/recipes.json?auth=' + token)
+      .map(
+        (recipes) => {
+          for (let recipe of recipes) {
+            if (!recipe['ingredients']) {
+              recipe['ingredients'] = [];
+            }
+          }
+          return recipes;
+        }
+      )
+      .subscribe(
+        (recipes: Recipe[]) => {
+          this.recipeService.setRecipes(recipes);
+        }
+      );
+  }
+}
